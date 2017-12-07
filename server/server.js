7a766d4e899ba9a36581e75b8310526e5fc85982
Jeremy Lincoln <bossco28@gmail.com>
2017-12-07 11:58:02 -0500

    Sending JSON Data

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 //non-express
 const http = require('http');
+const fs = require('fs');
+
+//HTML
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type':'text/html'});
+//     let HTML = fs.readFileSync(`${__dirname}/index.html`);
+//     res.end(HTML);
+// })
+
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type':'text/plain'
-    });
-    res.end('Hello dude, i am a response');
+    res.writeHead(200, {'Content-Type':'application/json'});
+    const names = ["Jeremy", "Brad", "Rhonda"]
+    const cars = {
+        name:"Ford",
+        model:"Fiesta"
+    }
+    const json = JSON.stringify({
+        names,
+        cars
+    })
+    //to display json data we need to turn into a string
+    res.end(json);
 })
-
 server.listen(8181, '127.0.0.1');
 console.log('Server is running');
+
+//console.log(__dirname);
